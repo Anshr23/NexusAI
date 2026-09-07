97675c6219ba2441c2619b31bbad3c14ed0a411a
JackAnsh23 <raiansh230405@gmail.com>
2026-09-07 21:46:33 +0530

    vite config simplyfying

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          mui: ['@mui/material', '@emotion/react', '@emotion/styled'],
-          router: ['react-router-dom'],
-          utils: ['axios', 'react-hot-toast', 'react-type-animation', 'react-icons'],
-          syntax: ['react-syntax-highlighter']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000 // Increase limit to 1000kb
-  }
 })
